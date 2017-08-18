9bd687a2dcad1d289f18bb5d09590293cc4d8c99
Tarun Singhal <tarunsinghaliitd@gmail.com>
2017-08-18 20:35:01 +0000

    ip stripping issue

--- api/controller/stats.js
+++ api/controller/stats.js
@@ -97,6 +97,7 @@ exports.addStats = function(req, res) {
 					'error': 'An error has occurred'
 				});
 			} else {
+				console.log(result);
 				res.send(result);
 			}
 		});
@@ -261,8 +262,15 @@ function addQuery(filter, params, query, default_val) {
 
 //get client IP
 function getClientIP(req) {
-	return req.headers['x-forwarded-for'] ||
+
+	var ip = req.headers['x-forwarded-for'] ||
 		req.connection.remoteAddress ||
 		req.socket.remoteAddress ||
 		req.connection.socket.remoteAddress;
+
+	if (ip.substr(0, 7) == "::ffff:") {
+		ip = ip.substr(7)
+	}
+
+	return ip
 };
