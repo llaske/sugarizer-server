2cc95e3a531bfcd12e30d82724080ddb648efacf
NikhilM98 <nmehra@ph.iitr.ac.in>
2019-04-02 21:33:53 +0530

    Removed redundant translation on change

--- dashboard/public/js/main.js
+++ dashboard/public/js/main.js
@@ -301,7 +301,6 @@ function onLocalized() {
 			lang.value = localStorage.getItem("languageSelection");
 		}
 		lang.onchange = function() {
-			l10n.setLanguage(this.value);
 			localStorage.setItem("languageSelection", this.value);
 			location.href = window.location.pathname + "?lang="+lang.value;
 		};
