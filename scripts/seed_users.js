0b018adcd1d555516f40e4ba636b618a1815223e
NikhilM98 <nmehra@ph.iitr.ac.in>
2019-04-13 04:10:46 +0530

    Seed data to database

--- scripts/seed_users.js
+++ scripts/seed_users.js
@@ -15,6 +15,15 @@ var csv = require('csv-parser');
 // Import fs module
 var fs = require('fs');
 
+// Import request library to make requests over the network
+var request = require('request');
+
+// Import common helper
+var common = require('../dashboard/helper/common');
+
+// Set the node enviornment to test
+process.env.NODE_ENV = 'test';
+
 // Define user with parameterized constructor
 class User {
     constructor(name, type, language, color, password, classroom) {
@@ -25,6 +34,9 @@ class User {
         this.password = password;
         if (classroom)
             this.classroom = classroom;
+        this.status = 0;
+        this.comment = "";
+        this._id = "";
     }
 }
 
@@ -33,7 +45,7 @@ var Users = [];
 
 // Trim and lowercase string
 function cleanString(string) {
-    if(!string) return;
+    if (!string) return;
     string = string.trim();
     string = string.toLowerCase();
     return string;
@@ -63,38 +75,318 @@ function generatePassword(length) {
 
 // Validate data row
 function validateUserRow(user, index) {
-    if(!user.name) {
+    if (!user.name) {
         console.log("Row: " + (index + 1) + " -> Username is invalid\nSkipping...");
         return;
     }
 
     user.type = cleanString(user.type);
-    if(!user.type) {
+    if (!user.type) {
         user.type = "student";
     }
 
     // ToDo: Validate language from an array of valid languages
     user.language = cleanString(user.language);
-    if(!user.language) {
+    if (!user.language) {
         user.language = "en";
     }
 
     user.color = cleanString(user.color);
-    if(!isValidColor(user.color)) {
+    if (!isValidColor(user.color)) {
         // ToDo: Randomize Color
-        user.color = '{"stroke":"#ff0000", "fill":"#0000ff"}';
+        user.color = '{ "stroke":"#ff0000", "fill":"#0000ff" }';
     }
 
     // ToDo: Get minimum password length from .ini file
-    if(!user.password || typeof user.password != "string" || user.password.length < 4) {
+    if (!user.password || typeof user.password != "string" || user.password.length < 4) {
         console.log("Row: " + (index + 1) + " -> User: " + user.name + " -- Password is invalid\nGenerating random password...");
         user.password = generatePassword(8);
     }
 
     user.classroom = user.classroom || "";
+    user.classroom = user.classroom.trim();
 
     return user;
 }
+
+function stringifyUser(user) {
+    return '{"name":"' + user.name + '", ' + 
+            '"color":' + user.color + ', ' +
+            '"role":"' + user.type + '", ' +
+            '"password":"' + user.password + '", ' + 
+            '"language":"' + user.language + '", ' +
+            '"options":{"sync":true, "stats":true}}';
+}
+
+// Initiate Master Admin
+var masterAdmin = stringifyUser(new User('Master_Admin_' + (+new Date()).toString(), 'admin', 'en', '{"stroke":"#ff0000", "fill":"#0000ff"}', 'password'));
+
+// Insert User
+function insertUser(i) {
+    return new Promise(function(resolve, reject) {
+        request({
+            headers: {
+                "content-type": "application/json",
+                "x-access-token": masterAdmin.token,
+                "x-key": masterAdmin.user._id
+            },
+            json: true,
+            method: 'post',
+            uri: common.getAPIUrl() + 'api/v1/users',
+            body: {
+                user: stringifyUser(Users[i])
+            }
+        }, function(error, response, body) {
+            if (response.statusCode == 200) {
+                Users[i].status = 1;
+                Users[i]._id = body._id;
+                resolve(body);
+            } else {
+                Users[i].comment = body.error;
+                reject(body);
+            }
+        });
+    });
+}
+
+function generateCSV() {
+    console.log("generating csv...");
+}
+
+// Initiate Classrooms
+var Classrooms = {}; // Contains {classname: [students_array]}
+
+function stringifyClassroom(name) {
+    var newStudents = Classrooms[name].students;
+    var oldStudents = [];
+    if (Classrooms[name].data && Classrooms[name].data.students && Classrooms[name].data.students.length > 0) {
+        oldStudents = Classrooms[name].data.students 
+    }
+    var union = [...new Set([...newStudents, ...oldStudents])];
+    var students = JSON.stringify(union);
+    console.log(union);
+    return '{"name":"' + name + '","color":{"stroke":"#FF0000","fill":"#0000FF"},"students":' + students + '}';
+}
+
+// Update classroom by ID
+function updateClassroom(name) {
+    return new Promise(function(resolve, reject) {
+        request({
+            headers: {
+                "content-type": "application/json",
+                "x-access-token": masterAdmin.token,
+                "x-key": masterAdmin.user._id
+            },
+            json: true,
+            method: 'put',
+            uri: common.getAPIUrl() + 'api/v1/classrooms/' + Classrooms[name].data._id,
+            body: {
+                classroom: stringifyClassroom(name)
+            }
+        }, function(error, response, body) {
+            body.q = name;
+            if (response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject(body);
+            }
+        });
+    });
+}
+
+// Insert Classroom
+function insertClassroom(name) {
+    return new Promise(function(resolve, reject) {
+        request({
+            headers: {
+                "content-type": "application/json",
+                "x-access-token": masterAdmin.token,
+                "x-key": masterAdmin.user._id
+            },
+            json: true,
+            method: 'post',
+            uri: common.getAPIUrl() + 'api/v1/classrooms',
+            body: {
+                classroom: stringifyClassroom(name)
+            }
+        }, function(error, response, body) {
+            body.q = name;
+            if (response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject(body);
+            }
+        });
+    });
+}
+
+// Find Classroom
+function findClassroom(name) {
+    return new Promise(function(resolve, reject) {
+        var query = {
+            sort: '+name',
+            q: name
+        };
+        request({
+            headers: {
+                "content-type": "application/json",
+                "x-access-token": masterAdmin.token,
+                "x-key": masterAdmin.user._id
+            },
+            json: true,
+            method: 'GET',
+            qs: query,
+            uri: common.getAPIUrl() + 'api/v1/classrooms'
+        }, function(error, response, body) {
+            body.q = name;
+            if (response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject(body);
+            }
+        });
+    });
+}
+
+// Create a list of all classrooms in Users
+function getClassroomsNamesFromUsers() {
+    var classroomList = [];
+    for(var i=0; i<Users.length; i++) {
+        if (Users[i].classroom && classroomList.indexOf(Users[i].classroom) == -1) classroomList.push(Users[i].classroom);
+    }
+    return classroomList;
+}
+
+// Find classrooms. Insert classroom if not found
+function findOrCreateClassroom(classes) {
+    var classroomProcessed = 0;
+    for (var i=0; i < classes.length; i++) {
+        findClassroom(classes[i]).then(function(res) {
+            if (res && res.classrooms && res.classrooms.length > 0 && res.classrooms[0] && res.q == res.classrooms[0].name) {
+                // Confirm Match
+                Classrooms[res.q].data = res.classrooms[0];
+                updateClassroom(res.q).then(function(res) {
+                    classroomProcessed++;
+                    if (res) {
+                        Classrooms[res.q].data = res;
+                    } else {
+                        console.log("error creating classroom");
+                    }
+                    if (classroomProcessed == classes.length) generateCSV();
+                })
+                .catch(function(err) {
+                    classroomProcessed++;
+                    console.log(err);
+                    if (classroomProcessed == classes.length) generateCSV();
+                })
+            } else {
+                // Create Classroom
+                insertClassroom(res.q).then(function(res) {
+                    classroomProcessed++;
+                    if (res) {
+                        Classrooms[res.q].data = res;
+                    } else {
+                        console.log("error creating classroom");
+                    }
+                    if (classroomProcessed == classes.length) generateCSV();
+                })
+                .catch(function(err) {
+                    classroomProcessed++;
+                    console.log(err);
+                    if (classroomProcessed == classes.length) generateCSV();
+                })
+            }
+        })
+        .catch(function(err) {
+            classroomProcessed++;
+            console.log(err);
+            if (classroomProcessed == classes.length) generateCSV();
+        });
+    }
+}
+
+// Processed users for assignment into classrooms
+function initClassroomAssignment() {
+    console.log('Initiating classroom assignment...');
+    var uniqueClassrooms = getClassroomsNamesFromUsers();
+    for (var i=0; i<uniqueClassrooms.length; i++) {
+        Classrooms[uniqueClassrooms[i]] = {data: "", students: []};
+    }
+    for (var j=0; j<Users.length; j++) {
+        if (Users[j].status && Users[j].type == "student" && Users[j]._id && Users[j].classroom && Classrooms[Users[j].classroom] && typeof Classrooms[Users[j].classroom].students == "object") {
+            // Push user into classroom
+            Classrooms[Users[j].classroom].students.push(Users[j]._id);
+        }
+    }
+
+    findOrCreateClassroom(uniqueClassrooms);
+}
+
+// Initiate Server
+function initServer() {
+    // Import sugarizer server
+    require('../sugarizer');
+}
+
+// Initiate seeding to DB
+function initSeed() {
+    // Create Temporary Admin
+    console.log('Creating Temporary Admin...');
+
+    request({
+        headers: {
+            "content-type": "application/json",
+        },
+        json: true,
+        method: 'POST',
+        uri: common.getAPIUrl() + 'auth/signup',
+        body: {
+            "user" : masterAdmin
+        }
+    }, function(error, response) {
+        if (response.statusCode == 200) {
+            console.log('Temporary Admin created');
+            request({
+                headers: {
+                    "content-type": "application/json",
+                },
+                json: true,
+                method: 'POST',
+                uri: common.getAPIUrl() + 'auth/login',
+                body: {
+                    "user" : masterAdmin
+                }
+            }, function(error, response, body) {
+                if (response.statusCode == 200) {
+                    masterAdmin = body;
+
+                    console.log('Logged into Temporary Admin');
+
+                    console.log('Inserting users...');
+                    var usersProcessed = 0;
+                    // Insert all users
+                    for (var i=0; i < Users.length; i++) {
+                        insertUser(i).then(function() {
+                            usersProcessed++;
+                            if (usersProcessed == Users.length) initClassroomAssignment();
+                        }).catch(function(err) {
+                            usersProcessed++;
+                            console.log(err);
+                            if (usersProcessed == Users.length) initClassroomAssignment();
+                        });
+                    }
+                } else {
+                    console.log('Error logging into Temporary Admin');
+                    process.exit(-1);
+                }
+            });
+        } else {
+            console.log('Error creating Temporary Admin');
+            process.exit(-1);
+        }
+    });
+}
+
 
 console.log("Reading and validating file");
 var dataIndex = 0;
@@ -114,5 +406,9 @@ fs.createReadStream(filename)
     .on('end', function() {
         console.log('Finished processing CSV file');
         console.log(Users);
+        console.log('Starting Sugarizer-Server...');
+        initServer();
+        setTimeout(initSeed, 1000);
     });
 
+// Bug: Issue while inserting users with same name;
