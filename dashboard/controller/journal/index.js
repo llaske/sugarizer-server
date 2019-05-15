29f1abb4b6730fc75e3fd566320bdd9e992694dc
Nikhil Mehra <nmehra@ph.iitr.ac.in>
2019-05-15 14:47:31 +0530

    Restructured dashboard/controller/journal

---
+++ dashboard/controller/journal/index.js
@@ -0,0 +1,44 @@
+// include libraries
+var moment = require('moment'),
+	common = require('../../helper/common'),
+	deleteEntry = require('./deleteEntry'),
+	getEntries = require('./getEntries');
+
+var _util = require('./util'),
+	getUsers = _util.getUsers;
+
+// init settings
+var ini = null;
+exports.init = function(settings) {
+	ini = settings;
+};
+
+exports.ini = function() {
+	return ini;
+};
+
+// main landing page
+exports.index = function(req, res) {
+
+	// reinit l10n and momemt with locale
+	common.reinitLocale(req);
+
+	getUsers(req, res, function(users) {
+		res.render('journal', {
+			module: 'journals',
+			moment: moment,
+			entries: [],
+			query: {
+				uid: -1,
+				type: 'private'
+			},
+			users: users,
+			account: req.session.user,
+			server: ini.information
+		});
+	});
+};
+
+exports.deleteEntry = deleteEntry;
+
+exports.getEntries = getEntries;
