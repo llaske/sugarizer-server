e59f0637365c2e2055fdcd9e2364783b92f1e3f0
Nikhil Mehra <nmehra@ph.iitr.ac.in>
2019-05-15 15:42:35 +0530

    Restructured dashboard/controller/auth

---
+++ dashboard/controller/auth/index.js
@@ -0,0 +1,20 @@
+// include libraries
+var getLogin = require('./getLogin'),
+	logout = require('./logout'),
+	postLogin = require('./postLogin'),
+	validateSession = require('./validateSession');
+
+// init settings
+var ini = null;
+exports.init = function(settings) {
+	ini = settings;
+};
+
+exports.ini = function() {
+	return ini;
+};
+
+exports.getLogin = getLogin;
+exports.logout = logout;
+exports.postLogin = postLogin;
+exports.validateSession = validateSession;
