75c7829a7299a0fb75bd2334e9bf0afb8cdc7e59
NikhilM98 <nmehra@ph.iitr.ac.in>
2019-07-10 01:00:17 +0530

    Export users as CSV

--- dashboard/route.js
+++ dashboard/route.js
@@ -34,6 +34,7 @@ module.exports = function(app, ini) {
 	app.get('/dashboard/users/add', authController.validateSession, usersController.addUser);
 	app.post('/dashboard/users/add', authController.validateSession, usersController.addUser);
 	app.post('/dashboard/users/import', upload.single('file'), authController.checkRole(usersController.importCSV));
+	app.get('/dashboard/users/export', authController.validateSession, usersController.exportCSV);
 	app.get('/dashboard/users/edit/:uid', authController.validateSession, usersController.editUser);
 	app.post('/dashboard/users/edit/:uid', authController.validateSession, usersController.editUser);
 	app.get('/dashboard/users/delete/:uid', authController.validateSession, usersController.deleteUser);
