54551310c0733440d9ea7756f807591434a43b3e
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-04-10 13:29:00 +0530

    remove debug command from start script

--- ansible/deploy-to-gcp/gcp-start-script.sh
+++ ansible/deploy-to-gcp/gcp-start-script.sh
@@ -6,7 +6,6 @@ curl -fsSL https://get.docker.com/ | sh
 curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd sugarizer-server
-touch testing.js
 sh generate-docker-compose.sh
 sleep 30s
 docker-compose up -d
