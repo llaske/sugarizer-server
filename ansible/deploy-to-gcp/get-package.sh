bd2a92450180128284fe5b566fa59c8577e72438
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-04-11 01:02:46 +0530

    removed install dependencies playbook

--- ansible/deploy-to-gcp/get-package.sh
+++ ansible/deploy-to-gcp/get-package.sh
@@ -8,8 +8,9 @@ sudo apt-get update
 sudo apt-get install -y subversion
 svn export https://github.com/$USER/$REPO/branches/$BRANCH/$RELPATH
 
-# Installing Ansible throug pip
+# Installing Ansible and other dependencies throug pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 pip install --user ansible
+pip install requests google-auth
 rm get-pip.py
