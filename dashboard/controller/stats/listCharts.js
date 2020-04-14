7a097d617cf803b88ec8c70efdb632c4ca368d04
NikhilM98 <nikhilmehra998@gmail.com>
2020-04-15 00:06:50 +0530

    Replaced request with superagent in stats controller

--- dashboard/controller/stats/listCharts.js
+++ dashboard/controller/stats/listCharts.js
@@ -1,5 +1,5 @@
 // include libraries
-var request = require('request'),
+var superagent = require('superagent'),
 	common = require('../../helper/common'),
 	chartList = require('./util/chartList')();
 
@@ -19,29 +19,27 @@ module.exports = function listCharts(req, res) {
 	}
 
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/charts'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
-			// send to list charts page
-			res.render('admin/listCharts', {
-				module: 'stats',
-				charts: body.charts,
-				headers: common.getHeaders(req),
-				chartList: chartDic,
-				account: req.session.user,
-				url: common.getAPIUrl(req),
-				server: stats.ini().information
-			});
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-			return res.redirect('/dashboard/stats');
-		}
-	});
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/charts')
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
+				// send to list charts page
+				res.render('admin/listCharts', {
+					module: 'stats',
+					charts: response.body.charts,
+					headers: common.getHeaders(req),
+					chartList: chartDic,
+					account: req.session.user,
+					url: common.getAPIUrl(req),
+					server: stats.ini().information
+				});
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+				return res.redirect('/dashboard/stats');
+			}
+		});
 };
