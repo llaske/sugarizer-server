e0bf04efd6693bab009385a3485db68f8b033fc5
NikhilM98 <nikhilmehra998@gmail.com>
2020-04-14 23:01:19 +0530

    Replaced request with superagent in activity controller

--- dashboard/controller/activities/index.js
+++ dashboard/controller/activities/index.js
@@ -1,5 +1,5 @@
 // include libraries
-var request = require('request'),
+var superagent = require('superagent'),
 	common = require('../../helper/common'),
 	fakeLaunch = require('./fakeLaunch'),
 	launch = require('./launch');
@@ -24,36 +24,34 @@ exports.index = function(req, res) {
 	}
 
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		qs: {
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/activities')
+		.set(common.getHeaders(req))
+		.query({
 			sort: sort,
 			name: (req.query.search ? req.query.search.trim() : undefined)
-		},
-		uri: common.getAPIUrl(req) + 'api/v1/activities'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
+		})
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
 
-			// send to activities page
-			res.render('activities', {
-				module: 'activities',
-				activities: body,
-				headers: common.getHeaders(req),
-				account: req.session.user,
-				url: common.getAPIUrl(req),
-				search: (req.query.search ? req.query.search.trim() : ''),
-				server: ini.information
-			});
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-			return res.redirect('/dashboard/activities');
-		}
-	});
+				// send to activities page
+				res.render('activities', {
+					module: 'activities',
+					activities: response.body,
+					headers: common.getHeaders(req),
+					account: req.session.user,
+					url: common.getAPIUrl(req),
+					search: (req.query.search ? req.query.search.trim() : ''),
+					server: ini.information
+				});
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+				return res.redirect('/dashboard/activities');
+			}
+		});
 };
 
 exports.fakeLaunch = fakeLaunch;
--- dashboard/controller/activities/util/index.js
+++ dashboard/controller/activities/util/index.js
@@ -1,37 +1,33 @@
 // include libraries
-var request = require('request'),
+var superagent = require('superagent'),
 	common = require('../../../helper/common');
 
 exports.getActivity = function(req, aid, callback) {
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/activities/' + aid
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
-			// callback
-			callback(body);
-		}
-	});
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/activities/' + aid)
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
+				// callback
+				callback(response.body);
+			}
+		});
 };
 
 exports.getUser = function(req, uid, callback) {
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/users/' + uid
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
-			// callback
-			callback(body);
-		} else {
-			callback();
-		}
-	});
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/users/' + uid)
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
+				// callback
+				callback(response.body);
+			} else {
+				callback();
+			}
+		});
 };
 
 exports.formQueryString = function(params) {
