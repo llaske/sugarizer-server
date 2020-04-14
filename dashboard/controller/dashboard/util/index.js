55e6f91124580c3aa235d5f1916574371b3f7354
NikhilM98 <nikhilmehra998@gmail.com>
2020-04-15 00:23:33 +0530

    Replaced request with superagent in dashboard controller

--- dashboard/controller/dashboard/util/index.js
+++ dashboard/controller/dashboard/util/index.js
@@ -1,92 +1,84 @@
 // include libraries
-var request = require('request'),
+var superagent = require('superagent'),
 	common = require('../../../helper/common');
 
 
 exports.getAllJournals = function(req, res, callback) {
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/journal'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/journal')
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
 
-			//callback
-			callback(body);
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-		}
-	});
+				//callback
+				callback(response.body);
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+			}
+		});
 };
 
 exports.getAllActivities = function(req, res, callback) {
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/activities'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/activities')
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
 
-			//callback
-			callback(body);
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-		}
-	});
+				//callback
+				callback(response.body);
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+			}
+		});
 };
 
 exports.getAllUsers = function(req, res, callback) {
 	var role = req.query.role || 'student';
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		qs: {
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/users')
+		.set(common.getHeaders(req))
+		.query({
 			role: role,
 			sort: '-timestamp',
 			limit: 100000000
-		},
-		uri: common.getAPIUrl(req) + 'api/v1/users'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
-			//callback
-			callback(body);
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-		}
-	});
+		})
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
+				//callback
+				callback(response.body);
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+			}
+		});
 };
 
 exports.getAllClassrooms = function(req, res, callback) {
 	// call
-	request({
-		headers: common.getHeaders(req),
-		json: true,
-		method: 'GET',
-		uri: common.getAPIUrl(req) + 'api/v1/classrooms'
-	}, function(error, response, body) {
-		if (response.statusCode == 200) {
+	superagent
+		.get(common.getAPIUrl(req) + 'api/v1/classrooms')
+		.set(common.getHeaders(req))
+		.end(function (error, response) {
+			if (response.statusCode == 200) {
 
-			//callback
-			callback(body);
-
-		} else {
-			req.flash('errors', {
-				msg: common.l10n.get('ErrorCode'+body.code)
-			});
-		}
-	});
+				//callback
+				callback(response.body);
+	
+			} else {
+				req.flash('errors', {
+					msg: common.l10n.get('ErrorCode'+response.body.code)
+				});
+			}
+		});
 };
