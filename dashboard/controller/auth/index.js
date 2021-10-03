4adea0a4bc92c5315d9f6cc86b854d30e245f46e
ricknjacky <jaikishanbrijwani@gmail.com>
2021-10-04 03:40:07 +0530

    add fullauth logic in post login

--- dashboard/controller/auth/index.js
+++ dashboard/controller/auth/index.js
@@ -3,6 +3,7 @@ var getLogin = require('./getLogin'),
 	logout = require('./logout'),
 	postLogin = require('./postLogin'),
 	validateSession = require('./validateSession'),
+	verify2FA = require('./verify2FA'),
 	checkRole = require('./checkRole');
 
 // init settings
@@ -20,3 +21,4 @@ exports.logout = logout;
 exports.postLogin = postLogin;
 exports.validateSession = validateSession;
 exports.checkRole = checkRole;
+exports.verify2FA = verify2FA;
