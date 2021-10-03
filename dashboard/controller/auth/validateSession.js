1933988a1c939b6eca0a0fdf3dfb009cd832bedc
ricknjacky <jaikishanbrijwani@gmail.com>
2021-10-04 03:40:42 +0530

    update validateSession logic

--- dashboard/controller/auth/validateSession.js
+++ dashboard/controller/auth/validateSession.js
@@ -5,6 +5,12 @@ module.exports = function validateSession(req, res, next) {
 	if (!req.session.user) {
 		return res.redirect('/dashboard/login');
 	}
-	if (req.session.user.user) req.role = req.session.user.user.role;
-	next();
+	if (req.session.user.partial === true){
+		return res.redirect('/dashboard/verify2FA');
+	} else if (req.session.user.partial === false && req.session.user.user && req.session.user.user.role) {
+		req.role = req.session.user.user.role;
+		next();
+	} else {
+		return res.redirect('/dashboard/login');
+	}
 };
