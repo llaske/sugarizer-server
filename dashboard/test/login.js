fbaa8f104e3b52eb4869707bc72b8b7668683cb2
parteekcoder <parteekgoyal118@gmail.com>
2023-03-12 23:54:00 +0530

    login tests

--- dashboard/test/login.js
+++ dashboard/test/login.js
@@ -1,6 +1,7 @@
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
+const expect  = require('chai').expect;
 var seleniumDriver = require('selenium-webdriver'),
 	mocha = require('mocha');
 var Builder = seleniumDriver.Builder,
@@ -23,6 +24,15 @@ describe('login', function(){
 		await driver.findElement(By.name('username')).sendKeys('teacher1');
 		await driver.findElement(By.name('password')).sendKeys('teacher1');
 		await driver.findElement(By.className('btn pull-right')).click();
+		expect(await driver.findElement(By.id('navbar-xo-icon'))).not.to.be.null;
+	});
+	it('should not login with incorrect credentials',async function() {
+        
+		await driver.get('http:localhost:8080/dashboard/login');
+		await driver.findElement(By.name('username')).sendKeys('teacher');
+		await driver.findElement(By.name('password')).sendKeys('teacher');
+		await driver.findElement(By.className('btn pull-right')).click();
+		expect(await driver.findElement(By.id('navbar-xo-icon'))).not.to.be.null;
 	});
 
 });
