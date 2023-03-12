0e1c4f5111d3f3e390d038fa009050283665c9ce
parteekcoder <parteekgoyal118@gmail.com>
2023-03-12 23:38:01 +0530

    fix eslint issues and refactored code

--- dashboard/test/login.js
+++ dashboard/test/login.js
@@ -1,25 +1,29 @@
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
+var seleniumDriver = require('selenium-webdriver'),
+	mocha = require('mocha');
+var Builder = seleniumDriver.Builder,
+	By = seleniumDriver.By,
+	Browser = seleniumDriver.Browser;
+var beforeEach = mocha.beforeEach,
+	afterEach = mocha.afterEach;
 
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-const { beforeEach, afterEach } = require('mocha');
-const {getAPIUrl} = require('../helper/common');
 var driver;
-beforeEach(async()=>{
-    driver = await new Builder().forBrowser(Browser.CHROME).build();
-})
-afterEach(async()=>{
-    await driver.quit();
-})
-describe('login',async function(){
-    it('should login with correct credentials',async function(){
+beforeEach(function(){
+	driver = new Builder().forBrowser(Browser.CHROME).build();
+});
+afterEach(function(){
+	driver.quit();
+});
+describe('login', function(){
+	it('should login with correct credentials',async function() {
         
-        await driver.get(`http:localhost:8080/dashboard/login`);
-        await driver.findElement(By.name('username')).sendKeys('teacher1');
-        await driver.findElement(By.name('password')).sendKeys('teacher1');
-        await driver.findElement(By.className('btn pull-right')).click();
-    })
+		await driver.get('http:localhost:8080/dashboard/login');
+		await driver.findElement(By.name('username')).sendKeys('teacher1');
+		await driver.findElement(By.name('password')).sendKeys('teacher1');
+		await driver.findElement(By.className('btn pull-right')).click();
+	});
 
-})
+});
 
